15a206279f9bc67a039ace11b0f1f18a99591b5c
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2024-03-19 20:37:45 +0100

    Up

--- flopp-kernel/build.gradle.kts
+++ flopp-kernel/build.gradle.kts
@@ -3,8 +3,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
     testImplementation("org.assertj:assertj-core:3.25.3")
 
-    testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.16.1")
-    testRuntimeOnly("ch.qos.logback:logback-classic:1.5.2")
+    testImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:2.16.2")
+    testRuntimeOnly("ch.qos.logback:logback-classic:1.5.3")
 }
 
 publishing {
