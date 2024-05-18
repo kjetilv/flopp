4b17419c56ad3cec6cc072b84a90146bced67e92
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2024-05-18 12:29:14 +0200

    Bummer

--- buildSrc/src/main/kotlin/com/github/kjetilv/flopp/bld/Native.kt
+++ buildSrc/src/main/kotlin/com/github/kjetilv/flopp/bld/Native.kt
@@ -53,6 +53,7 @@ object Native {
          --gc=$gc
          -H:Optimize=3
          -H:+ReportExceptionStackTraces
+         -H:+ForeignAPISupport
          -o $toBinary
          -march=native
         """.trimIndent()
