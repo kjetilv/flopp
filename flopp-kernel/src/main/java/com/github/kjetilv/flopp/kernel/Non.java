d5f27266febcbbf04b5f9406df5f388fc6561239
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2024-08-05 21:19:22 +0200

    CleanupsCleanups

--- flopp-kernel/src/main/java/com/github/kjetilv/flopp/kernel/Non.java
+++ flopp-kernel/src/main/java/com/github/kjetilv/flopp/kernel/Non.java
@@ -13,66 +13,54 @@ public final class Non {
     }
 
     public static int negativeOrZero(int i, String name) {
-        if (pos(i)) {
+        if (i > 0) {
             return i;
         }
         throw new IllegalStateException("Expected non-zero " + name + ": " + i);
     }
 
     public static short negativeOrZero(short i, String name) {
-        if (pos(i)) {
+        if (i > 0) {
             return i;
         }
         throw new IllegalStateException("Expected non-zero " + name + ": " + i);
     }
 
     public static long negativeOrZero(long l, String name) {
-        if (pos(l)) {
+        if (l > 0) {
             return l;
         }
         throw new IllegalStateException("Expected non-zero " + name + ": " + l);
     }
 
     public static double negativeOrZero(double d, String name) {
-        if (pos(d)) {
+        if (d > 0) {
             return d;
         }
         throw new IllegalStateException("Expected non-zero " + name + ": " + d);
     }
 
     public static int negative(int i, String name) {
-        if (neg(i)) {
+        if (i < 0) {
             throw new IllegalStateException("Expected non-negative " + name + ": " + i);
         }
         return i;
     }
 
     public static short negative(short i, String name) {
-        if (neg(i)) {
+        if (i < 0) {
             throw new IllegalStateException("Expected non-negative " + name + ": " + i);
         }
         return i;
     }
 
     public static long negative(long l, String name) {
-        if (neg(l)) {
+        if (l < 0) {
             throw new IllegalStateException("Expected non-negative " + name + ": " + l);
         }
         return l;
     }
 
     private Non() {
-    }
-
-    private static boolean pos(long l) {
-        return l > 0;
-    }
-
-    private static boolean pos(double d) {
-        return Math.signum(d) > .5;
-    }
-
-    private static boolean neg(long l) {
-        return l < 0;
     }
 }
